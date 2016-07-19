c03efdf8c5f161af2638a9dd2c5f1747b62c03d0
wickerplane <wilgre@wharton.upenn.edu>
2016-07-18 23:41:28 -0400

    Changed posts_list to load Posts as items in client-side

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -1,17 +1,5 @@
-var postsData = [
-	{
-		title: 'Introducing Telescope',
-		url: 'http://sachagreif.com/introducing-telescope/'
-	},
-	{
-		title: 'Meteor',
-		url: 'http://meteor.com'
-	},
-	{
-		title: 'The Meteor Book',
-		url: 'http://meteor.com'
+Template.postList.helpers({
+	posts: function() {
+		return Posts.find();
 	}
-];
-Template.postsList.helpers({
-	posts: postsData
-});
+})
