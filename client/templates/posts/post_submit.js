4f7d6357824effd6612df9394e92bba00d257aba
wickerplane <wilgre@wharton.upenn.edu>
2016-07-31 16:06:22 -0400

    Demonstrate the order that posts appear using a sleep

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -3,15 +3,24 @@ Template.postSubmit.events({
 		e.preventDefault();
 
 		var post = {
-			url: $(e.target).find('[name=url').val(),
+			url: $(e.target).find('[name=url]').val(),
 			title: $(e.target).find('[name=title]').val()
 		};
 
-		Meteor.call('postInsert', post, function(error,result) {
-			if (error);
+		Meteor.call('postInsert', post, function(error, result) {
+			// display the error to the user and abort
+			if (error)
 				return alert(error.reason);
 
-			Router.go('postPage', {_id: result._id});
+			// show this result but route anyway
+			if (result.postExists)
+				alert('This link has already been posted');
+
+
 		});
+
+		Router.go('postsList');
+
+
 	}
 });
