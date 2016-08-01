778cd61b087ff2f27a91413810d24b37a0f82309
wickerplane <wilgre@wharton.upenn.edu>
2016-08-01 00:05:37 -0400

    Added basic permission to check the post's owner.

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -1,7 +1,11 @@
 Template.postItem.helpers ({
+	ownPost: function() {
+		console.log("FUCK ME IN THE ASSHOLE - NICK MANCINI");
+		return this.userId === Meteor.userId();
+	},
 	domain: function() {
 		var a = document.createElement('a');
 		a.href = this.url;
 		return a.hostname;
 	}
-	});
+});
